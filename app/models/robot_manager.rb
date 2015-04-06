2dcf030b96fff4f299e31eb2d2dd99da5ce7df1c
Carmer <carmerandrew@gmail.com>
2015-04-06 09:31:57 -0600

    cleaned up comments

--- app/models/robot_manager.rb
+++ app/models/robot_manager.rb
@@ -42,7 +42,6 @@ class RobotManager
 
   def self.robot_info(id)
     robo_database.from(:robots).where(:id => id).to_a.first
-    # robots_info.find { |robot| robot["id"] == id }
   end
 
   def self.find(id)
@@ -56,12 +55,6 @@ class RobotManager
       @robo_database ||= Sequel.sqlite("db/robot_manager_development.sqlite3")
     end
   end
-
-  # def self.robots_info
-  #   robo_database.transaction do
-  #     robo_database['robots'] || []
-  #   end
-  # end
 
   def self.all
     data = robo_database.from(:robots).to_a
@@ -78,16 +71,6 @@ class RobotManager
     :date_hired => robot[:date_hired],
     :department => robot[:department]
     )
-    # robo_database.transaction do
-    #   to_change = robo_database['robots'].find { |robot| robot["id"] == id }
-    #   to_change["name"] = robot[:name]
-    #   to_change["city"] = robot[:city]
-    #   to_change["state"] = robot[:state]
-    #   to_change["avatar"] = robot[:avatar]
-    #   to_change["birthday"] = robot[:dob]
-    #   to_change["date_hired"] = robot[:date_hired]
-    #   to_change["department"] =  robot[:department]
-    # end
   end
 
   def self.destroy(id)
